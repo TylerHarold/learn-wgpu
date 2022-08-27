303788d055ce83323aba04a301cc219926345b3c
Tyler Harold <tyler@programmer.net>
2022-08-27 17:01:26 -0400

    add start of state

--- src/lib.rs
+++ src/lib.rs
@@ -2,13 +2,15 @@ use winit::{
     event::*,
     event_loop::{ControlFlow, EventLoop},
     window::WindowBuilder,
+    window::Window,
 };
 
 #[cfg(target_arch="wasm32")]
 use wasm_bindgen::prelude::*;
 
+
 #[cfg_attr(target_arch="wasm32", wasm_bindgen(start))]
-pub fn run() {
+pub async fn run() {
     /*
     *   It is very important to enable logging via env_logger::init();. When gpu hits any
     *   error it panics with a generic message, while logging the real error via the log
@@ -34,6 +36,9 @@ pub fn run() {
 
     let event_loop = EventLoop::new();
     let window = WindowBuilder::new().build(&event_loop).unwrap();
+
+    // After we build the window, create a mutable state
+    let mut state = State::new(&window).await;
 
     event_loop.run(move |event, _, control_flow| match event {
         Event::WindowEvent {
@@ -75,5 +80,132 @@ pub fn run() {
                 Some(())
             })
             .expect("Couldn't append canvas to document body.");
+    }
+}
+
+struct State {
+    surface: wgpu::Surface,
+    device: wgpu::Device,
+    queue: wgpu::Queue,
+    config: wgpu::SurfaceConfiguration,
+    size: winit::dpi::PhysicalSize<u32>,
+}
+
+impl State {
+    // Creating some of the wgpu types requires async code
+    async fn new(window: &Window) -> Self {
+        let size = window.inner_size();
+
+        // The instance is a handle to our GPU
+        // Backends::all => Vulkan + Metal + DX12 + Browser WebGPU
+        let instance = wgpu::Instance::new(wgpu::Backends::all());
+
+        // The surface is the part of the window we draw to.
+        let surface = unsafe {
+            instance.create_surface(window)
+        };
+
+        // The adapter is the handle to our graphics card.
+        // We can use this to get information about the graphics card
+        // including its name and what backend the adapter uses. We will
+        // use this to create our Device & Queue later.
+        let adapter = instance.request_adapter(
+            &wgpu::RequestAdapterOptions {
+                // power_preference has two variants, LowPower, and HighPerformance.
+                power_preference: wgpu::PowerPreference::default(),
+                // compatible_surface field tells wgpu to find an adapter that can present
+                // to the supplied surface.
+                compatible_surface: Some(&surface),
+                // force_fallback_adapter forces wgpu to pick an adapter that will work on
+                // all hardware. This usually means that the rendering backend will use a
+                // "software" system, instead of hardware such as a GPU.
+                force_fallback_adapter: false,
+            },
+        ).await.unwrap();
+
+        // The options passed to request_adapter aren't guaranteed to work for all devices,
+        // but will work for most of them. If wgpu can''t find an adapter with the required
+        // permissions, request_adapter will return None. If you want to get all the adapters
+        // for a particular backend you can use enumerate_adapters. This will give you an
+        // iterator that you cna loop over to check if one of the adapters work for your needs.
+        //
+        // Another thing to note is that Adapters are locked to a specific backend. If you are
+        // on Windows and have 2 graphics cards you will have at least 4 adapters available to use.
+        
+        /*
+            let adapter = instance
+                .enumerate_adapters(wgpu::Backends::all())
+                .filter(|adapter| {
+                    // Check if this adapter supports our surface
+                    surface.get_preferred_format(&adapter).is_some()
+                })
+                .next()
+                .unwrap()
+        */
+
+        // Use the adapter to create the device and queue.
+        let (device, queue) = adapter.request_device(
+            &wgpu::DeviceDescriptor {
+                features: wgpu::Features::empty(),
+                // WebGL doesn't support all of wgpu's features, so if we're building for
+                // the web we'll have to disable some.
+                limits: if cfg!(target_arch = "wasm32") {
+                    wgpu::Limits::downlevel_webgl2_defaults()
+                } else {
+                    wgpu::Limits::default()
+                },
+                label: None,
+            },
+            None, // Trace path
+        ).await.unwrap();
+
+        // Surface config
+        let config = wgpu::SurfaceConfiguration {
+            // Usage field will describe how SurfaceTexture(s) will be used. RENDER_ATTACHMENT
+            // specifices that the textures will be used to write to the screen.
+            usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+            // Format describes how SurfaceTexture(s) will be stored on the gpu. We use
+            // get_preferred_format(&adapter) to figure out the best format to use based on the
+            // display you're using.
+            format: surface.get_supported_formats(&adapter)[0],
+            // Width & height are the width & height in pixels of a SurfaceTexture. This should
+            // usually be the width and height of the window. Don't set this to 0, this WILL crash lol.
+            width: size.width,
+            height: size.height,
+            // present_mode uses wgpu::PresentMode enum which determines how to sync the surface with
+            // the display. The option we picked, PresentMode::Fifo, will cap the display rate at the
+            // display's framerate (essentially VSync). This mode is guaranteed to be supoorted on all platforms.
+
+            // If we want to let our users pick what PresentMode they use, you can use Surface::get_supported_modes()
+            // to get a list of all the PresentModes the surface supports
+            // let modes = surface.get_supported_modes(&adapter);
+            present_mode: wgpu::PresentMode::Fifo,
+        };
+        surface.configure(&device, &config);
+
+
+        Self {
+            surface,
+            device,
+            queue,
+            config,
+            size,
+        }
+    }
+
+    fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>) {
+        todo!()
+    }
+
+    fn input(&mut self, event: &WindowEvent) -> bool {
+        todo!()
+    }
+
+    fn update(&mut self) {
+        todo!()
+    }
+
+    fn render(&mut self) -> Result<(), wgpu::SurfaceError> {
+        todo!()
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
 use learn_wgpu::run; 
 
+/*
+*   Since our run is async, main() will need some way to await the future. We coukld use a crate
+*   like tokio or async-std, but this example we'll go with the much more lightweight pollster. 
+*/
 fn main() {
-    run();
+    // Don't use block_on inside of an async function if we plan to support WASM. Futures have to
+    // be run using the browser's executor. If you try to bring your own your code will crash
+    // when you encounter a future that doesn't execute immediately.
+    pollster::block_on(run());
 }
