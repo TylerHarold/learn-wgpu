eb7ff0452f3c04fae12893313bfba2db198b5af2
Tyler Harold <tyler@programmer.net>
2022-08-27 17:59:54 -0400

    add resize & render method + update event loop

--- src/lib.rs
+++ src/lib.rs
@@ -44,19 +44,50 @@ pub async fn run() {
         Event::WindowEvent {
             ref event,
             window_id,
-        } if window_id == window.id() => match event {
-            WindowEvent::CloseRequested
-            | WindowEvent::KeyboardInput {
-                input:
-                    KeyboardInput {
-                        state: ElementState::Pressed,
-                        virtual_keycode: Some(VirtualKeyCode::Escape),
-                        ..
-                },
-                ..
-            } => *control_flow = ControlFlow::Exit,
-            _ => {}
-        },
+        } if window_id == window.id() => if !state.input(event) {
+            match event {
+                WindowEvent::CloseRequested
+                | WindowEvent::KeyboardInput {
+                    input:
+                        KeyboardInput {
+                            state: ElementState::Pressed,
+                            virtual_keycode: Some(VirtualKeyCode::Escape),
+                            ..
+                    },
+                    ..
+                } => *control_flow = ControlFlow::Exit,
+
+                WindowEvent::Resized(physical_size) => {
+                    state.resize(*physical_size);
+                }
+
+                WindowEvent::ScaleFactorChanged { new_inner_size, .. } => {
+                    // new_inner_size is &&mut so we have to dereference it twice
+                    state.resize(**new_inner_size);
+                }
+
+                
+                _ => {}
+
+            }
+        }
+        Event::RedrawRequested(window_id) if window_id == window.id() => {
+            state.update();
+            match state.render() {
+                Ok(_) => {}
+                // Reconfigure the surface if lost
+                Err(wgpu::SurfaceError::Lost) => state.resize(state.size),
+                // The system is out of memory, we should probably quit
+                Err(wgpu::SurfaceError::OutOfMemory) => *control_flow = ControlFlow::Exit,
+                // All other errors
+                Err(e) => eprintln!("{:?}", e),
+            }
+        }
+
+        Event::MainEventsCleared => {
+            // RedrawRequest will only trigger once, unless we manually request it
+            window.request_redraw();
+        }
         _ => {}
     });
 
@@ -193,19 +224,72 @@ impl State {
         }
     }
 
+    // Handles window resizing
     fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>) {
-        todo!()
+        if new_size.width > 0 && new_size.height > 0 {
+            self.size = new_size;
+            self.config.width = new_size.width;
+            self.config.height = new_size.height;
+            self.surface.configure(&self.device, &self.config);
+        }
     }
 
+    // Returns a bool to indicate whether an event has been fully processed. If the method returns true,
+    // the main loop won't process the event any further.
+
+    // TODO: We're just going to return false for now because we don't have any events we want to capture.
     fn input(&mut self, event: &WindowEvent) -> bool {
-        todo!()
+        false
     }
 
     fn update(&mut self) {
-        todo!()
+        // yeah.  
     }
 
     fn render(&mut self) -> Result<(), wgpu::SurfaceError> {
-        todo!()
+        // First we need to get a frame to render to
+        // the get_current_texture function will wait for the surface to provide a new SurfaceTexture that
+        // we will render to. We'll store this in output for later.
+        let output = self.surface.get_current_texture()?;
+
+        // view creates a TextureView with default settings. We need to do this because we want to control
+        // how the render code interacts with the texture.
+        let view = output.texture.create_view(&wgpu::TextureViewDescriptor::default());
+
+        // We also need to create a CommandEncoder to create the actual commands to send to the gpu. Most
+        // modern graphics frameworks expect commands to be stored in a command buffer before being sent
+        // to the gpu. The encoder builds a command buffer that we can then send to the gpu.
+        let mut encoder = self.device.create_command_encoder(&wgpu::CommandEncoderDescriptor {
+            label: Some("Render Encoder"),
+        });
+
+        // Extra block borrows encoder mutably (aka &mut self). We can't call encoder.finish() until
+        // we release that mutable borrow. The block tells rust to drop any variables within it when the
+        // code leaves that scope thus releasing the mutable borrow on encoder and allowing us to finish() it.
+        {
+            let _render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+                label: Some("Render Pass"),
+                color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+                    view: &view,
+                    resolve_target: None,
+                    ops: wgpu::Operations {
+                        load: wgpu::LoadOp::Clear(wgpu::Color {
+                            r: 0.1,
+                            g: 0.2,
+                            b: 0.3,
+                            a: 1.0,
+                        }),
+                        store: true,
+                    }
+                })],
+                depth_stencil_attachment: None,
+            });
+        }
+
+        // submit will accept anything that implements IntoIter
+        self.queue.submit(std::iter::once(encoder.finish()));
+        output.present();
+
+        Ok(())
     }
 }
